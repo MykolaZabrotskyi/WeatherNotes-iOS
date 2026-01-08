5da71689a6bcbd8968de883b595dbcdb708e6f83
MykolaZabrotskyi <zabrotskyi.mykola@gmail.com>
2026-01-08 20:02:07 +0200

    Changed View to NavigationStack

--- WeatherNotes/WeatherNotes/Sources/Views/ListScreen.swift
+++ WeatherNotes/WeatherNotes/Sources/Views/ListScreen.swift
@@ -9,45 +9,61 @@ import SwiftUI
 
 struct ListScreen: View {
     var body: some View {
-        VStack {
-            HStack {
-                Spacer()
-                Text(Metrics.Title.text)
-                    .fontWeight(.bold)
-                    .font(Font.system(size: Metrics.Title.fontSize))
-                    .foregroundStyle(Metrics.color)
-                Spacer()
-                Button(action: {
-                } ) {
-                    Label("", systemImage: Metrics.AddButton.systemImage)
-                        .font(Font.system(size: Metrics.AddButton.fontSize))
-                        .foregroundStyle(Metrics.color)
-                } // Button
-                Spacer()
-            } // HStack
-            ScrollView {
-                NoteCard()
-                NoteCard()
-                NoteCard()
-                NoteCard()
-            } // ScrollView
-        } // VStack
+        NavigationStack {
+            VStack {
+                ScrollView {
+                    NoteCard()
+                    NoteCard()
+                    NoteCard()
+                    NoteCard()
+                } // ScrollView
+            } // VStack
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text(Metrics.Title.text)
+                        .padding(.leading, Metrics.Title.padding)
+                        .font(Font.system(
+                            size: Metrics.Title.fontSize,
+                            weight: Metrics.Title.fontWeight)
+                        )
+                        .foregroundStyle(Metrics.Title.color)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+
+                    } label: {
+                        Image(systemName: Metrics.AddButton.systemImage)
+                            .font(.system(
+                                size: Metrics.AddButton.fontSize,
+                                weight: Metrics.AddButton.fontWeight
+                            ))
+                            .foregroundStyle(Metrics.AddButton.color)
+                            .padding(Metrics.AddButton.padding)
+                    }
+                }
+            }
+        } // NavigationStack
     }
 }
 
 private enum Metrics {
     enum Title {
         static let text = "WeatherNotes"
+        static let padding = 5.0
         static let fontSize = 35.0
         static let fontWeight: Font.Weight = .bold
         static let color: Color = .indigo
     }
     
     enum AddButton {
-        static let systemImage = "plus.capsule.fill"
-        static let fontSize = 60.0
+        static let systemImage = "plus"
+        static let fontWeight: Font.Weight = .bold
+        static let fontSize = 30.0
+        static let color: Color = .indigo
+        static let padding = 20.0
     }
-
+    
     static let color: Color = .indigo
 }
 
