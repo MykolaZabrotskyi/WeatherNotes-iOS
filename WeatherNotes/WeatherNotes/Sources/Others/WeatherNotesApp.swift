1289c509a81964e19b3cfc9f1ec98a7402207b2f
MykolaZabrotskyi <zabrotskyi.mykola@gmail.com>
2026-01-08 15:36:44 +0200

    First commit

---
+++ WeatherNotes/WeatherNotes/Sources/Others/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Mykola Zabrotskyi on 08.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+
+        }
+    }
+}
