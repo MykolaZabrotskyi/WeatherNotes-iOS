7546bc46fff90b0966e745339cbafdf194d25704
MykolaZabrotskyi <zabrotskyi.mykola@gmail.com>
2026-01-08 19:18:43 +0200

    Add AddNoteView layout and toolbar

--- WeatherNotes/WeatherNotes/Sources/Views/AddNoteView.swift
+++ WeatherNotes/WeatherNotes/Sources/Views/AddNoteView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 struct AddNoteView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack() {
+                
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        
+                    }
+                        .foregroundStyle(Metrics.Buttons.color)
+                        .font(Font.system(
+                            size: Metrics.Buttons.fontSize,
+                            weight: Metrics.Buttons.fontWeight
+                        ))
+                }
+                ToolbarItem(placement: .principal) {
+                    Text("Add Note")
+                        .font(Font.system(
+                            size: Metrics.Title.fontSize,
+                            weight: Metrics.Title.fontWeight
+                        ))
+                        .foregroundStyle(Metrics.Title.color)
+                        .padding(.horizontal, Metrics.Title.Frame.horizontalPadding)
+                        .padding(.vertical, Metrics.Title.Frame.verticalPadding)
+                        .background(
+                            RoundedRectangle(cornerRadius: Metrics.cornerRadius)
+                                .fill(Metrics.Title.Frame.color)
+                        )
+                }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        
+                    }
+                    .foregroundStyle(Metrics.Buttons.color)
+                    .font(Font.system(
+                        size: Metrics.Buttons.fontSize,
+                        weight: Metrics.Buttons.fontWeight
+                    ))
+                }
+            }
+        }
     }
+}
+
+private enum Metrics {
+    enum Title {
+        static let fontSize = 30.0
+        static let fontWeight: Font.Weight = .semibold
+        static let color: Color = .white
+        enum Frame {
+            static let horizontalPadding = 16.0
+            static let verticalPadding = 8.0
+            static let color: Color = .indigo
+        }
+    }
+    
+    enum Buttons {
+        static let fontSize = 25.0
+        static let fontWeight: Font.Weight = .regular
+        static let color: Color = .indigo
+    }
+    
+    static let cornerRadius = 25.0
 }
 
 #Preview {
