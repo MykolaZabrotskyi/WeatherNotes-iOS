7a0ce00ac87ac3daa6066c277953ac1f3f356bf7
MykolaZabrotskyi <zabrotskyi.mykola@gmail.com>
2026-01-08 19:16:55 +0200

    Add AddNoteView UI with navigation bar.

---
+++ WeatherNotes/WeatherNotes/Sources/Views/AddNoteView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNoteView.swift
+//  WeatherNotes
+//
+//  Created by Mykola Zabrotskyi on 08.01.2026.
+//
+
+import SwiftUI
+
+struct AddNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNoteView()
+}
